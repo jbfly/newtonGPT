584e36aae7ec10a2800b9287a91f612d2f9e70fb
jbfly <john.bonewitz@gmail.com>
2023-04-23 21:22:18 -1000

    Commented Code

--- chatgpt_asyncio_telnet_api.py
+++ chatgpt_asyncio_telnet_api.py
@@ -1,34 +1,43 @@
-# chatgpt_asyncio_telnet.py
+# Import necessary libraries
 import asyncio
 import openai
 import os
 import aiohttp
 
+# Define an asynchronous function to call the ChatGPT API with the conversation history
 async def call_chatgpt_api(conversation_history):
+    # Set the headers for the API request
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {openai.api_key}",
     }
 
+    # Set the API URL
     url = "https://api.openai.com/v1/chat/completions"
 
+    # Prepare the payload for the API request
     payload = {
         "model": "gpt-3.5-turbo",
         "messages": conversation_history,
     }
 
+    # Make an asynchronous POST request to the API
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, json=payload) as response:
             response_data = await response.json()
 
+    # Extract the ChatGPT reply from the response data
     chatgpt_reply = response_data['choices'][0]['message']['content']
     return chatgpt_reply
 
+# Define an asynchronous function to handle incoming Telnet connections
 async def handle_connection(reader, writer):
+    # Get the address of the connected client
     addr = writer.get_extra_info('peername')
 
     print(f'Connection from {addr}')
 
+    # Send a welcome message to the connected client
     writer.write(b'Hello, Newton! Type your message and press Enter to chat with ChatGPT.\r\n')
 
     # Initialize the conversation history
@@ -36,24 +45,28 @@ async def handle_connection(reader, writer):
         {"role": "system", "content": "You are a helpful assistant."},
     ]
 
+    # Main loop to handle incoming messages from the client
     while True:
+        # Read data from the client until a newline character is encountered
         data = await reader.readuntil(b'\n')
         if not data:
             break
 
+        # Decode the received message and print it
         message = data.decode('ascii', 'replace').strip()
         print(f'Received {message!r} from {addr}')
 
-        # Add user message to conversation history
+        # Add the user message to the conversation history
         conversation_history.append({"role": "user", "content": message})
 
-        # Call the ChatGPT API and get the response
+        # Call the ChatGPT API with the updated conversation history and get the response
         chatgpt_reply = await call_chatgpt_api(conversation_history)
         print(f"ChatGPT Reply: {chatgpt_reply}")
 
         # Add ChatGPT's reply to the conversation history
         conversation_history.append({"role": "assistant", "content": chatgpt_reply})
 
+        # Split the reply into lines and send it to the client
         reply_lines = chatgpt_reply.split('\n')
         
         # Print "ChatGPT: " only once at the beginning of the message
@@ -67,20 +80,23 @@ async def handle_connection(reader, writer):
         # Add a newline after the response
         writer.write(b'\r\n')
 
+    # Close the connection with the client
     print(f'Closing the connection with {addr}')
     writer.close()
 
-
-
+# Define the main asynchronous function
 async def main():
+    # Start an asyncio server to handle incoming Telnet connections
     server = await asyncio.start_server(handle_connection, '0.0.0.0', 6801)
 
+    # Get the server address and print it
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
 
+    # Run the server in the context of an asynchronous context manager
     async with server:
         await server.serve_forever()
 
+# Run the main function when the script is executed
 if __name__ == '__main__':
     asyncio.run(main())
-
